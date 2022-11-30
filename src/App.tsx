45fb7c0541f1abdfb4a1f7ab3cb12574108655bc
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2022-11-29 18:20:37 -0800

    add CredentialView component
    Signed-off-by: Clécio Varjão <1348549+cvarjao@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@
  * @format
  */
 
-import React, {type PropsWithChildren} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -27,35 +27,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import CredentialView from './lib/components/CredentialView';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -78,19 +50,19 @@ const App = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
+          <CredentialView title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
+          </CredentialView>
+          <CredentialView title="See Your Changes">
             <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
+          </CredentialView>
+          <CredentialView title="Debug">
             <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
+          </CredentialView>
+          <CredentialView title="Learn More">
             Read the docs to discover what to do next:
-          </Section>
+          </CredentialView>
           <LearnMoreLinks />
         </View>
       </ScrollView>
