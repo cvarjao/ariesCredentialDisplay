a010416e1b8c1788cf7bacc49c751ecf045b8ef0
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2022-11-29 20:09:32 -0800

    add yarn workspaces
    more:https://alexhunt.io/notes/react-native-yarn-workspaces
    Signed-off-by: Clécio Varjão <1348549+cvarjao@users.noreply.github.com>

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,17 +1,56 @@
 /**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
+ * Metro Bundler configuration
+ * https://facebook.github.io/metro/docs/en/configuration
  *
- * @format
+ * eslint-env node, es6
  */
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const getWorkspaces = require('get-yarn-workspaces');
+const path = require('path');
+
+function getConfig(appDir, options = {}) {
+  const workspaces = getWorkspaces(appDir);
+
+  // Add additional Yarn workspace package roots to the module map
+  // https://bit.ly/2LHHTP0
+  const watchFolders = [
+    path.resolve(appDir, '..', 'node_modules'),
+    ...workspaces.filter(workspaceDir => !(workspaceDir === appDir)),
+  ];
+  console.log(`watchFolders:`, watchFolders)
+  const extraNodeModules = {
+    // Resolve all react-native module imports to the locally-installed version
+    'react-native': path.resolve(appDir, 'node_modules', 'react-native'),
+
+    // Resolve additional nohoist modules depended on by other packages
+    'react-native-svg': path.resolve(
+      appDir,
+      'node_modules',
+      'react-native-svg',
+    ),
+
+    // Resolve core-js imports to the locally installed version
+    'core-js': path.resolve(appDir, 'node_modules', 'core-js'),
+  }
+  console.log(`extraNodeModules:`, extraNodeModules)
+  return {
+    watchFolders,
+    resolver: {
+      blockList: exclusionList([
+        // Ignore other resolved react-native installations outside of
+        // myapp-native - this prevents a module naming collision when mapped.
+        // /^((?!myapp-native).)+[\/\\]node_modules[/\\]react-native[/\\].*/,
+
+        // Ignore react-native-svg dependency in myapp-ui, mapped below.
+        // react-native-svg must only be included once due to a side-effect. It
+        // has not been hoisted as it requires native module linking here.
+        // http://bit.ly/2LJ7V4b
+        // /myapp-ui[\/\\]node_modules[/\\]react-native-svg[/\\].*/,
+      ]),
+      extraNodeModules,
+    },
+  };
+}
+
+module.exports = getConfig(__dirname);
