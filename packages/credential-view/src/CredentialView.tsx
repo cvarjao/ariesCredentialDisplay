94368e04f11192890fa5a81666d80a2b0ee91e52
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2022-11-30 12:12:38 -0800

    Working / FlexBox 1
    Signed-off-by: Clécio Varjão <1348549+cvarjao@users.noreply.github.com>

--- packages/credential-view/src/CredentialView.tsx
+++ packages/credential-view/src/CredentialView.tsx
@@ -27,49 +27,56 @@ const CredentialView: React.FC<CredentialCardProps> = ({
       style={[
         {width: width, backgroundColor: layout.primary_background_color},
       ]}>
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <View style={{flex: 1, height: 15}} />
-        <View style={{flex: 1, flexDirection: 'row', height: 15}}>
-          <View style={{flex: 1, backgroundColor: '#E52B50'}}>
-            <Text>{'1.1'}</Text>
-          </View>
-          <View style={{flex: 1, backgroundColor: '#e82c51'}}>
-            <Text>{'2'}</Text>
-          </View>
-          <View style={{flex: 1, backgroundColor: '#c92646'}}>
-            <Text>{'3'}</Text>
-          </View>
-          <View style={{flex: 1, backgroundColor: '#a8203b'}}>
-            <Text>{'4'}</Text>
-          </View>
-          <View style={{flex: 10, backgroundColor: '#C9FFE5'}}>
-            <Text>{'5'}</Text>
+      <View style={{flex: 1, flexDirection: 'row', padding: 10}}>
+        <View style={{flex: 4}}>
+          <View style={{flex: 1, flexDirection: 'column'}}>
+            <View style={{flex: 0, height: 15, backgroundColor: 'cyan'}} />
+            <View style={{flex: 1, height: 60}}>
+              <View style={{flex: 1, flexDirection: 'row'}}>
+                <View style={{flex: 1, backgroundColor: 'cyan'}} />
+                <View style={{flex: 2, minHeight: 50}}><Text>{"1"}</Text></View>
+                <View style={{flex: 1, backgroundColor: 'cyan'}} />
+              </View>
+            </View>
           </View>
         </View>
-        <View style={{flex: 1, flexDirection: 'row', height: 15}}>
-          <View style={{flex: 1, backgroundColor: '#E52B50'}}>
-            <Text>{'1.2'}</Text>
-          </View>
-          <View style={{flex: 1, backgroundColor: '#e82c51'}}>
-            <Text>{'2'}</Text>
-          </View>
-          <View style={{flex: 1, backgroundColor: '#c92646'}}>
-            <Text>{'3'}</Text>
-          </View>
-          <View style={{flex: 1, backgroundColor: '#a8203b'}}>
-            <Text>{'4'}</Text>
-          </View>
-          <View style={{flex: 10, backgroundColor: '#C9FFE5'}}>
-            <Text>{'5'}</Text>
-          </View>
+        <View
+          style={{
+            flex: 14,
+            flexDirection: 'column',
+            backgroundColor: '#C9FFE5',
+          }}>
+            <View style={{flex: 0, minHeight: 42, flexDirection: 'row'}}>
+              <View style={{flex: 12}}>
+                <View style={{flex: 0, height: 15, backgroundColor: 'cyan'}}/>
+                <View style={{flex: 0, height: 20, backgroundColor: 'yellow'}}><Text style={styles.fieldLabelText}>{"Government of British Columbia"}</Text></View>
+                <View style={{flex: 0, minHeight: 22, backgroundColor: 'yellow'}}><Text style={styles.fieldValueText}>{"Business License"}</Text></View>
+              </View>
+              <View style={{flex: 2, minHeight: 52, backgroundColor: 'red'}}/>
+            </View>
+            <View style={{flex: 0, height: 20, backgroundColor: 'green'}} ><Text style={styles.fieldLabelText}>{"Business Name:"}</Text></View>
+            <View style={{flex: 0, minHeight: 22, backgroundColor: 'yellow'}}><Text style={styles.fieldValueText}>{"Some Extremely Ultra Mega Long Business's Name"}</Text></View>
+            <View style={{flex: 0, height: 20, backgroundColor: 'green'}} ><Text style={styles.fieldLabelText}>{"Business Type:"}</Text></View>
+            <View style={{flex: 0, minHeight: 22, backgroundColor: 'yellow'}}><Text style={styles.fieldValueText}>{"Some Extremely Ultra Mega Long Business's Type"}</Text></View>
+            <View style={{flex: 0, height: 15, backgroundColor: 'cyan'}} />
         </View>
-        <View style={{flex: 1, height: 15}} />
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  fieldLabelText: {
+    fontFamily: 'verdana',
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+  fieldValueText: {
+    fontFamily: 'verdana',
+    fontWeight: 'bold',
+    fontSize: 16,
+    flexWrap: 'wrap',
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
